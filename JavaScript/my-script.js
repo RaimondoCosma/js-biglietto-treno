9dc6dd785e40edcdd08b87e36381aafde95f22f0
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-20 13:49:46 +0200

    Aggiunge codice js funzionante in console.log

--- JavaScript/my-script.js
+++ JavaScript/my-script.js
@@ -18,4 +18,20 @@ console.log(`La tua età è di: ${age} anni`);
 
 // 3. Imposto il prezzo del biglietto
 let trainPrice = myDistance * 0.21
-console.log(`Il prezzo totale del biglietto è di: ${trainPrice}€`);
+console.log(`Il prezzo totale del biglietto è di: ${trainPrice.toFixed(2)}€`);
+
+
+// 4. Imposto le variabili di sconto
+const youngCardPrice = trainPrice * 0.2;
+const seniorCardPrice = trainPrice * 0.4;
+
+// 5. Imposto le condizioni per attuare lo sconto
+if (age < 18) {
+    trainPrice = trainPrice - youngCardPrice;
+    console.log(`Ha diritto ad uno sconto del 20% come possessore della Carta Young: Prezzo = ${trainPrice.toFixed(2)}€`)
+} else if (age >= 65){
+    trainPrice = trainPrice - seniorCardPrice;
+    console.log(`Ha diritto ad uno sconto del 20% come possessore della Carta Senior: Prezzo = ${trainPrice.toFixed(2)}€`)
+} else {
+    console.log(`Non abbiamo piani di sconto per la sua fascia di età: Prezzo = ${trainPrice.toFixed(2)}€`)
+}
